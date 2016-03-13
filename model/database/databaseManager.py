cf02cd8eea83da35f244e664497c4e2fb06225de
lukasmarshall <lukasmarshall@gmail.com>
2016-03-14 09:56:20 +1100

    printing file to see if server is iterating properly

--- model/database/databaseManager.py
+++ model/database/databaseManager.py
@@ -115,6 +115,7 @@ class DatabaseManager():
 		timeseries = None
 		fileList = os.listdir(folder)
 		for f in fileList:
+			print f
 			if f == fileName:
 				print "Existing dataset found. Reading from file."
 				timeseries = np.loadtxt(folder +"/"+f ,dtype='float', delimiter = ",",skiprows=0, usecols=None, unpack=False)
